5a49f6fe1aa4b4cc6f4c32b951f03fa2714adcce
wiramc2111 <wiramc2111@student.faytechcc.edu>
2026-02-04 10:59:02 -0500

    Add files via upload

---
+++ P1LAB1_Wiram_Chad.py
@@ -0,0 +1,13 @@
+# Chad Wiram
+# 2//4/2026
+# PLAB1
+# Welcome to CTI-110
+
+# Create Variables
+first_name = input("Enter your First Name: ")
+last_name = input("Enter your Last Name: ")
+
+#Display Welcome message using f string
+print()
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
+print()
